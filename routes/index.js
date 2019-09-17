95fe2f12643bb3e621f08660652b133b52e3cc0d
Apoorv Jain <apoorv9496@gmail.com>
2019-09-17 20:23:48 +0530

    added node-readability in-place of mozilla's readability

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var reader = require('./Readability.js');
-var JSDOM = require('jsdom').JSDOM;
-var fetch = require('node-fetch');
+//var reader = require('./Readability.js');
+//var JSDOM = require('jsdom').JSDOM;
+//var fetch = require('node-fetch');
+var read = require('node-readability');
 const {getMetadata} = require('page-metadata-parser');
 
 
@@ -14,9 +15,41 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.post('/view', async function(req, res, next) {
   
+  // url passed in body
   console.log(req.body.url);
 
-  const response = await fetch(req.body.url);
+  let metadata = null;
+
+  read(req.body.url, function(err, article, meta) {
+
+    // Main Article
+    console.log(article.content);
+    // Title
+    console.log(article.title);
+
+    metadata = getMetadata(article.document, req.body.url);
+
+    res.status(200).send({
+      "article": article.content,
+      "meta": metadata
+    });
+
+    // Close article to clean up jsdom and prevent leaks
+    article.close();
+  });
+});
+
+module.exports = router;
+
+// HTML Source Code
+    //console.log(article.html);
+    // DOM
+    //console.log(article.document);
+
+    // Response Object from Request Lib
+    //console.log(meta);
+
+/*const response = await fetch(req.body.url);
   const html = await response.text();
   var doc = new JSDOM(html, {
     url: req.body.url,
@@ -28,7 +61,7 @@ router.post('/view', async function(req, res, next) {
   try {
 
     metadata = getMetadata(doc.window.document, req.body.url);
-    //console.log(metadata);
+    console.log(metadata);
     article = new reader(doc.window.document).parse();
 
     if(article !== null)
@@ -36,12 +69,4 @@ router.post('/view', async function(req, res, next) {
   } catch (error) {
 
     console.log(error);
-  }
-
-  res.status(200).send({
-    "article": content,
-    "meta": metadata
-  });
-});
-
-module.exports = router;
+  }*/
